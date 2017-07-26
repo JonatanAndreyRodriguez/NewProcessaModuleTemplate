9bb836e158f57a1a74d35fac212e44161df87377
R&D Processa <RD-Processa@users.noreply.github.com>
2017-07-26 11:52:01 -0500

    Update Get-ConfigFile.ps1

--- Functions/Get-ConfigFile.ps1
+++ Functions/Get-ConfigFile.ps1
@@ -1,7 +1,7 @@
 function Get-ConfigFile {
-<#
+    <#
 .SYNOPSIS
-Obtiene la ruta de acceso del archivo de configuración.
+Obtiene la ruta de acceso del archivo de configuraciÃ³n.
 
 .DESCRIPTION
 Si existiera un archivo Debug se devuelve la ruta de ese archivo. De lo contrario se utliza la ruta predeterminada.
@@ -18,30 +18,33 @@ Ruta de acceso en -Path o un archivo con el nombre .Debug si existiera.
 'C:\MyConfig.Debug.config' si existiera, de lo contrario 'C:\MyConfig.config'
 
 .NOTES
-Autor: <%=$PLASTER_PARAM_ModuleAuthor%>
+Autor: Atorrest
 #>
     [CmdletBinding()]
     [OutputType([string])]
     Param
     (
         [Parameter(Mandatory, ValueFromPipeline)]
-        [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
+        [string]
         $Path
     )
 
     try {
+        if (Test-Path -Path $Path) {
 
-        $Extension = [System.IO.Path]::GetExtension($Path)
-        $DebugFile = [System.IO.Path]::ChangeExtension($Path, '.Debug{0}' -f $Extension)
-    
-        if (Test-Path -Path $DebugFile -PathType Leaf) {
-            $DebugFile | Write-Output
-            return
+            $Extension = [System.IO.Path]::GetExtension($Path)
+            $DebugFile = [System.IO.Path]::ChangeExtension($Path, '.Debug{0}' -f $Extension)
+
+            if (Test-Path -Path $DebugFile -PathType Leaf) {
+                $DebugFile | Write-Output
+                return
+            }
+
+            $Path | Write-Output
         }
-
-        $Path | Write-Output
     }
     catch {
         throw
     }
 }
+
