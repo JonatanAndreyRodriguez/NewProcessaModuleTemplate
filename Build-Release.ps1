eedac16e2d9ef7b70706339c03b70acf54c49978
Alvaro Torres Tatis <alvaro.torres.tatis@gmail.com>
2017-08-02 09:43:19 -0500

    Script para generar el archivo Release
    Se implementa script para generar el archivo Release

---
+++ Build-Release.ps1
@@ -0,0 +1,5 @@
+﻿$Manifest = [xml](Get-Content -Path "$pwd\PlasterManifest.xml" -Raw)
+$Version = $Manifest.plasterManifest.metadata.version
+$DestinationPath = Join-Path -Path $env:TEMP -ChildPath "NewProcessaModuleTemplate_$Version.zip"
+Compress-Archive -Path $pwd -DestinationPath $DestinationPath
+$DestinationPath | Out-Default
